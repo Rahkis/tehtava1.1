04843cc554df428e4b3011d1135b83a8acdd347f
Rahkis <44240232+Rahkis@users.noreply.github.com>
2018-11-16 18:59:15 +0200

    tehtava1_8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button =({ handleClick, text}) => (
+    <button onClick ={handleClick}>
+    {text}
+    </button>    
+)
+
+const Statistics = ({hyva, neutraali, huono}) =>{
+    return(
+        <div>
+        <div> Hyvät:  {hyva}</div>            
+        <div> Neutraalit:  {neutraali}</div>
+        <div> Huonot:  {huono}</div>
+        </div>
+        )
+}
+
+const Statistic =({ nimi, arvo, extra})=>{
+    return(
+        <div>{nimi} : {arvo} {extra}</div>
+    )
+}
+    
+
+
 
 class App extends React.Component{
     constructor(){
@@ -10,9 +34,7 @@ class App extends React.Component{
             neutraali: 0,
             huono:0
         }
-    }
-
-   
+    }   
 
 asetaHyva = () => {
     this.setState({
@@ -34,6 +56,8 @@ asetaHuono = () =>{
 }
 
 
+
+
 render() {
 
     const summa = this.state.hyva - this.state.huono
@@ -48,17 +72,26 @@ render() {
             Anna Palautetta
             </h1>
         <div>
-          <button onClick={this.asetaHyva}>Hyvä</button>
-          <button onClick={this.asetaNeutraali}>Neutraali</button>
-          <button onClick={this.asetaHuono}>Huono</button>
+        <Button 
+                handleClick={this.asetaHyva}
+                 text = "Hyvä"       
+        />
+        <Button 
+                handleClick={this.asetaNeutraali}
+                 text = "Neutraali"       
+        />
+        <Button 
+                handleClick={this.asetaHuono}
+                 text = "Huono"       
+        />
+          
         </div>
         <div><h2>Tulokset </h2> </div>
             
-            <div> Hyvät:  {this.state.hyva}</div>            
-            <div> Neutraalit:  {this.state.neutraali}</div>
-            <div> Huonot:  {this.state.huono}</div>
-            <div> Keskiarvo:  {keskiarvo.toFixed(2)}</div>
-            <div> Positiivisia:  {pos.toFixed(2)} %</div>
+            <Statistics hyva = {this.state.hyva} neutraali = {this.state.neutraali} huono={this.state.huono} />           
+            <Statistic nimi="Keskiarvo" arvo={keskiarvo.toFixed(2)} />
+            <Statistic nimi="Positiivisia" arvo={pos.toFixed(2)} extra="%" />
+            
         
     </div>
       )
